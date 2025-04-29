757f5bb153bc2363409d99fe9954261ae7f24348
HiroLiang <hiromichi.liang@gmail.com>
2025-04-29 23:18:08 +0800

    Edit BaseSteps

--- src/test/java/com/hiro/cathay/test/base/BaseSteps.java
+++ src/test/java/com/hiro/cathay/test/base/BaseSteps.java
@@ -2,21 +2,26 @@ package com.hiro.cathay.test.base;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public abstract class BaseSteps {
 
     protected <T> List<T> toList(List<Map<String, String>> rows, Class<T> clazz) {
-        try {
-            List<T> list = new ArrayList<>();
-            for (Map<String, String> row : rows) {
-                T instance = clazz.getDeclaredConstructor().newInstance();
-                for (Map.Entry<String, String> entry : row.entrySet()) {
-                    String fieldName = entry.getKey();
-                    String value = entry.getValue();
+            return rows.stream()
+                    .map(row -> convert(row, clazz))
+                    .collect(Collectors.toList());
+    }
 
+    protected <T> T convert(Map<String, String> row, Class<T> clazz) {
+        try {
+            T instance = clazz.getDeclaredConstructor().newInstance();
+            for (Map.Entry<String, String> entry : row.entrySet()) {
+                String fieldName = entry.getKey();
+                String value = entry.getValue();
+
+                try {
                     String setterName = "set" + Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
                     for (Method method : clazz.getMethods()) {
                         if (method.getName().equals(setterName) && method.getParameterCount() == 1) {
@@ -26,10 +31,11 @@ public abstract class BaseSteps {
                             break;
                         }
                     }
+                } catch (Exception e) {
+                    System.out.println("Field: " + fieldName + " is not found in class: " + clazz.getSimpleName());
                 }
-                list.add(instance);
             }
-            return list;
+            return instance;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException("Convert class: " + clazz.getSimpleName() + " failed.", e);
         }
